4bb8751eaf71353013bd4a41c8b4f05e9daaa035
i.lukyanov <i.lukyanov@galaksion.com>
2022-12-26 22:00:56 +0500

    base example refactor

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,10 +3,6 @@ import generatePicker from 'antd/es/date-picker/generatePicker';
 import dateFnsGenerateConfig from 'rc-picker/lib/generate/dateFns';
 import React from 'react';
 
-import 'antd/es/date-picker/style/index';
-
-const { TabPane } = Tabs;
-
 const DatePicker = generatePicker<Date>(dateFnsGenerateConfig);
 
 const Main = () => {
@@ -14,18 +10,26 @@ const Main = () => {
         <>
             <Row gutter={5} justify="center" style={{ margin: '10px' }}>
                 <Col span={24}>
-                    <Tabs defaultActiveKey="1" size="middle" type="card">
-                        <TabPane key="1" tab="Card Tab 1">
-                            <div style={{ height: '200px', backgroundColor: 'rosybrown' }}>1</div>
-                        </TabPane>
-                        <TabPane key="2" tab="Card Tab 2">
-                            Content of card tab 2
-                        </TabPane>
-                        <TabPane key="3" tab="Card Tab 3">
-                            Content of card tab 3
-                            <DatePicker.RangePicker onChange={console.log} />
-                        </TabPane>
-                    </Tabs>
+                    <Tabs defaultActiveKey="1" size="middle" type="card" items={[
+                        {
+                            key: '1',
+                            label: 'Card tab 1',
+                            children: <div style={{ height: '200px', backgroundColor: 'rosybrown' }}>1</div>,
+                        },
+                        {
+                            key: '2',
+                            label: 'Card tab 2',
+                            children: 'Content of tab 2',
+                        },
+                        {
+                            key: '3',
+                            label: 'Card tab 3',
+                            children: <>
+                                Content of card tab 3{' '}
+                                <DatePicker.RangePicker onChange={console.log} />
+                            </>,
+                        },
+                    ]}  />
                 </Col>
             </Row>
         </>
